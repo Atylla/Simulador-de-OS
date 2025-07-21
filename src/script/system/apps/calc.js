533c65c08ff8fe47a69abff199a1c3e13978ca92
Atila Araujo <atilabh96@gmail.com>
2025-07-21 11:06:22 -0400

    fix: consertando bugs da calculadora, operação de porcentagem ainda não implementada

--- src/script/system/apps/calc.js
+++ src/script/system/apps/calc.js
@@ -1,6 +1,7 @@
 export const renderCalc = () => {
     const painel = document.querySelector('#painel');
     const btnNumber = document.querySelectorAll('button[data-number]');
+    const btnCaract = document.querySelectorAll('button[data-caract]');
 
     let valorAtual = '';
 
@@ -15,7 +16,39 @@ export const renderCalc = () => {
             painel.innerHTML = valorAtual;
         });
     })
-    
+
+    btnCaract.forEach( c => {
+        c.addEventListener('click', () => {
+            const caract = c.getAttribute('data-caract');
+
+            if( caract === '=') {
+                try {
+                    const expressao = valorAtual
+                        .replace(/,/g, '.')
+                        //.replace(/(\d+(\.\d+)?)%/g, '($1/100)');
+
+                    const result = eval(expressao);
+                    valorAtual = result.toString();
+                    painel.innerHTML = valorAtual;
+                } catch (e) {
+                    painel.innerHTML = 'erro';
+                    valorAtual = '';
+                }
+                return;
+            } else if (caract === 'c') {
+                valorAtual = valorAtual.slice(0, -1);
+                painel.innerHTML = valorAtual || '0';
+                return;
+            } else if ( caract === '%') {
+                painel.innerHTML = 'Operação ainda não implementada'
+                return;
+            }
+
+            valorAtual += caract;
+
+            painel.innerHTML = valorAtual;
+        })
+    })
 
 }
 
